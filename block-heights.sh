c652ba0f71bb327607c122a560a84e1e4cf3624d
Jamie Wilkinson <jamie@jamiedubs.com>
2020-09-18 09:57:09 -0400

    Script to check miner vs. official block heights

---
+++ block-heights.sh
@@ -0,0 +1,8 @@
+# echo; echo "blockchain-etl:"
+# $HOME/blockchain-etl/blockchain_etl info height
+
+echo; echo "miner:"
+docker exec miner miner info height
+
+echo; echo "Helium API:"
+curl -s https://api.helium.io/v1/stats | jq -r '.data .counts .blocks'
