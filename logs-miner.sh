9a23c1b645728d8832fbae6382171efe32038eda
Jamie Wilkinson <jamie@jamiedubs.com>
2020-09-21 14:48:35 -0400

    Use 'tail -F' so logging continues post-file rotation

--- logs-miner.sh
+++ logs-miner.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker exec miner tail -f -n0 /var/log/miner/console.log /var/log/miner/error.log /var/log/miner/crash.log
+docker exec miner tail -F -n0 /var/log/miner/console.log /var/log/miner/error.log /var/log/miner/crash.log
